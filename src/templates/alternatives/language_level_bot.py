f4cf8948525a07bf58f8151ea72c024a13049051
Mihaela Baksic <mihaela.baksic@doxray.com>
2023-08-08 09:27:39 +0000

    Prompt alternatives developed and tested

--- src/templates/alternatives/language_level_bot.py
+++ src/templates/alternatives/language_level_bot.py
@@ -1,11 +1,14 @@
 language_level_refinement_alternatives = [
         # Basic instruction
+        # Arguments: language_level, cefr_description
         "Rephrase the text to be on {0} level of CEFR language proficiency: {1}",
 
         # Basic instruction with language level description
-        "Rephrase the text to be on {0} level of CEFR language proficiency. The {0} level is defined as: {1}. Rephrase the text to be on a {0} level: {2}",
+        # Arguments: language_level, cefr_description
+        "Rephrase the text to be on {0} level of CEFR language proficiency. The {0} level is defined as: {1}. Rephrase the text to be on a {0} level: ",
 
         # Instruction + language level description + CoT
-        "Rephrase the text to be on {0} level of CEFR language proficiency. The {0} level is defined as: {1}.\n\nExamples of reasoning:\n\nSentence:  There is no way this can be achieved in just under 20 minutes, they must have gone mad.\nLevel: A1\nThought: Is there too complex vocabulary? Can the grammar be simplified? Can the most basic user understand this?\nRephrasing: They could not do this in 20 minutes. They are crazy.\n\nSentence:  Football matches can create great atmosphere in stadiums because football is competitive.\nLevel:  C1\nThought: Can the grammar and vocabulary be improved? Does it fit C1 knowledge?  Rephrasing: The intensity and competitiveness of football matches can create an electrifying atmosphere in stadiums.\n\nRephrase the text to be on a {0} level: {2} Only output the rephrasing."
+        # Arguments: language_level, cefr_description
+        "Rephrase the text to be on {0} level of CEFR language proficiency. The {0} level is defined as: {1}.\n\nExamples of reasoning:\n\nSentence:  There is no way this can be achieved in just under 20 minutes, they must have gone mad.\nLevel: A1\nThought: Is there too complex vocabulary? Can the grammar be simplified? Can the most basic user understand this?\nRephrasing: They could not do this in 20 minutes. They are crazy.\n\nSentence:  Football matches can create great atmosphere in stadiums because football is competitive.\nLevel:  C1\nThought: Can the grammar and vocabulary be improved? Does it fit C1 knowledge?  Rephrasing: The intensity and competitiveness of football matches can create an electrifying atmosphere in stadiums.\n\nRephrase the text to be on a {0} level. Only output the rephrasing."
 
     ]
---
+++ src/templates/alternatives/role_fitness_bot.py
@@ -0,0 +1,11 @@
+role_fitness_refinement_alternatives = [
+    
+    # Arguments: GPT_role
+    'Act as a {0}. Rephrase the sentence to sound more like a {0}, while keeping the meaning: ',
+    
+    # Arguments: GPT_role, user_role
+    "Act as a {0}. You are speaking to {1}. Make minimal changes to the tone and voice of the provided sentence to sound more like {0}: " ,
+    
+    # Arguments: GPT_role, user_role, setting
+    "Rephrase the sentence to match the tone and voice of a {0} speaking to {1} in a setting described as {2}:"
+]
