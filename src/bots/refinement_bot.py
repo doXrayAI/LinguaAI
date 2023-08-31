c626405a1c41d2e8d2365da025c0319f44c40917
Mihaela Baksic <mihaela.baksic@doxray.com>
2023-08-31 08:40:13 +0000

    Refinement bots loading prompts from template alternatives

--- src/bots/refinement_bot.py
+++ src/bots/refinement_bot.py
@@ -1,17 +1,21 @@
 from bot import Bot
 from utils import load_template, load_cefr
 from time import sleep
+from src.templates.alternatives.role_fitness_bot import role_fitness_refinement_alternatives
+from src.templates.alternatives.role_fitness_language_level_bot import role_fitness_language_level_refinement_alternatives
+from src.templates.alternatives.language_level_bot import language_level_refinement_alternatives
 
 class LanguageLevelRefinementBot(Bot):
     '''Refining the input to match given language level'''
     
-    def __init__(self, language_level):
+    def __init__(self, language_level, alternative):
         super().__init__()
         self.__template = dict()
-        self.__template = load_template('language_level_refinement')
+        #self.__template = load_template('language_level_refinement')
+        self.__template = language_level_refinement_alternatives[alternative]
         language_level_description = load_cefr(language_level)
         
-        self._prompt_builder.add_template(self.__template['template'], (language_level, language_level_description))
+        self._prompt_builder.add_template(self.__template, (language_level, language_level_description))
         
         
     def send(self, args, history) :
@@ -29,17 +33,22 @@ class LanguageLevelRefinementBot(Bot):
 class RoleFollowingBot(Bot):
     '''Refining the input to match the GPT and user role'''
     
-    def __init__(self, GPT_role, user_role, setting):
+    def __init__(self, GPT_role, user_role, setting, alternative=0):
         super().__init__()
         self.__template = dict()
-        self.__template = load_template('role_following_refinement')
-        self._prompt_builder.add_template(self.__template['template'], (GPT_role, user_role, setting))
+        
+        #self.__template = load_template('role_following_refinement')
+        self.__template = role_fitness_refinement_alternatives[alternative]
+        
+        print(self.__template)
+        
+        self._prompt_builder.add_template(self.__template, (GPT_role, user_role, setting))
         
         
     def send(self, args, history) :
         '''Accepts the input string as args, stores refinement to history and returns the refinement.'''
         
-        sleep(5)
+        sleep(20)
 
         self._prompt_builder.add_template(args)
         prompt = self._prompt_builder.build()
@@ -77,13 +86,15 @@ class UserEngagementBot(Bot):
     
 class RoleFitnessLanguageLevelBot(Bot):
     
-    def __init__(self, GPT_role, user_role, language_level):
+    def __init__(self, GPT_role, user_role, language_level, alternative=0):
+        
         super().__init__()
         self.__template = dict()
-        self.__template = load_template('role_fitness_language_level_refinement')
+        #self.__template = load_template('role_fitness_language_level_refinement')
+        self.__template = role_fitness_language_level_refinement_alternatives[alternative]
         
         cefr_description = load_cefr(language_level)
-        self._prompt_builder.add_template(self.__template['template'], ( language_level, cefr_description, GPT_role, user_role))
+        self._prompt_builder.add_template(self.__template, ( language_level, cefr_description, GPT_role, user_role))
         
         
     def send(self, args, history) :
--- src/templates/alternatives/role_fitness_bot.py
+++ src/templates/alternatives/role_fitness_bot.py
@@ -10,5 +10,5 @@ role_fitness_refinement_alternatives = [
     "Rephrase the sentence to match the tone and voice of a {0} speaking to {1} in a setting described as {2}:",
     
     # Arguments: GPT_role, user_role,
-    "Act as a {0}. You are speaking to {1}. Make minimal changes to the tone and voice of the provided sentence to sound more like {0}\n Examples:\nWaiter: Hi, what do you want?-> Waiter: Hi, what can I offer you today?\nPoliceman: Lady, what happened here? -> Policeman: Ma'am, could you rpovide more information about the situation?"
+    "Act as a {0}. You are speaking to {1}. Make minimal changes to the tone and voice of the provided sentence to sound more like {0}\n Examples:\nWaiter: Hi, what do you want?-> Waiter: Hi, what can I offer you today?\nPoliceman: Lady, what happened here? -> Policeman: Ma'am, could you provide more information about the situation?"
 ]
