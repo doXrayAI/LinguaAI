c23f14cdf5b3deaa56ff762d2feaa2b6ccf12aa9
Mihaela Baksic <mihaela.baksic@doxray.com>
2023-09-07 09:57:09 +0000

    Web page design

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
 import {pr, show_hide_invalid_setting_message, show_roles } from './validation.js'
 
 
+let language_global;
+let language_level_global;
+let role_object_global;
+
+
 
 window.onload = function () {
     show_hide_invalid_setting_message(true);
@@ -17,11 +22,17 @@ window.onload = function () {
 
       let url = `/context/${user_language}/${language_level}/${setting_description}`
 
+      console.log(url)
+
+      language_global = user_language
+      language_level_global = language_level
+
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
             let valid_state = data.content
             console.log(valid_state)
+
             show_hide_invalid_setting_message(valid_state)
 
             if(valid_state){
@@ -34,7 +45,11 @@ window.onload = function () {
                     .then((data) => {
                         let roles = data.content
                         console.log(roles)
-                        show_roles(roles)
+
+                        //show_roles(roles)
+                        
+                        role_object_global = roles
+
                     })
                 .catch((error) => {
                     console.error("Error getting roles:", error)
@@ -46,24 +61,19 @@ window.onload = function () {
           });
     });
 
-    document
-    .getElementById("chat-form")
-    .addEventListener("submit", function (event) {
-        // Prevent the form from submitting and refreshing the page
-        event.preventDefault();
-
-        let userInput = document.getElementById("user-input").value;
-        let url = `/gpt4?user_input=${encodeURIComponent(userInput)}`;
-
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            let content = data.content;
-            let resultDiv = document.getElementById("result");
-            resultDiv.innerHTML = content;
-        })
-        .catch((error) => {
-            console.error("Error fetching GPT response:", error);
-        });
-    });
 };
+
+
+$(function(){
+    $(".heading-compose").click(function() {
+      $(".side-two").css({
+        "left": "0"
+      });
+    });
+
+    $(".newMessage-back").click(function() {
+      $(".side-two").css({
+        "left": "-100%"
+      });
+    });
+}) 
--- static/js/validation.js
+++ static/js/validation.js
@@ -8,11 +8,11 @@ function pr(){
 
 function show_hide_invalid_setting_message(valid = True){
 
-    document.getElementById("error-message").textContent = "";
+    document.getElementById("description-error").textContent = "";
 
     if (!valid) {
         // Display an error message in red
-        document.getElementById("error-message").textContent = "Setting Description is not valid. Please try something else.";
+        document.getElementById("description-error").textContent = "Setting description is not valid. Please try something else.";
         // clear the input
         document.getElementById("setting-description").value = ""
     }
@@ -21,8 +21,8 @@ function show_hide_invalid_setting_message(valid = True){
 
 function show_roles(role_object){
 
-    let elem = document.getElementById("roles")
-    elem.textContent = `Bot takes the role of ${role_object.GPT_role}. Your role is ${role_object.user_role}.`
+    document.getElementById("roles").textContent = `Bot takes the role of ${role_object.GPT_role}. Your role is ${role_object.user_role}.`
+    document.getElementById("setting").textContent = `The setting of your conversation is: ${role_object.setting}`
 }
 
 
